04eab0751b39d7f41f0fa5cbd62d9335e6e02a2c
Jesse <jr2g17@soton.ac.uk>
2023-12-05 16:32:56 +0000

    Updating remove class

--- restaurant.py
+++ restaurant.py
@@ -13,7 +13,7 @@ class Table:
 
     def remove(self, item, price, quantity=1):
         for item_order in self.bill:
-            if item and price in item_order:
+            if item_order["item"] == item and item_order["price"] == price:
                 item_order["quantity"] -= quantity
                 if quantity == 0:
                     del self.bill[item_order]
