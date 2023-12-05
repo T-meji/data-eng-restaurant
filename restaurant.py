e8aaec46b56dbb3046f751ddcc119087bf526959
Anushkriti Sarvesh <asarvesh0818@gmail.com>
2023-12-05 15:58:10 +0000

    pushing order class

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,20 @@
 class Table:
-    pass
+    def __init__(self,num):
+        self.bill = []
+        self.number = num
+
+    def order(self,item,price,quantity=1):
+        for objects in self.bill:
+            if objects["item"] == item and objects["price"] == price:
+                objects["quantity"] += quantity
+        else:
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
+
+    def remove(self):
+        pass
+    def get_subtotal(self):
+        pass
+    def get_total(self):
+        pass
+    def split_bill(self):
+        pass
