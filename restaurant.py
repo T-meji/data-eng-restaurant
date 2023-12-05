b554b916af3a3dd21b7efaef63b7e58ec88b41f2
Jesse <jr2g17@soton.ac.uk>
2023-12-05 14:38:38 +0000

    Testing git push

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,13 @@
 class Table:
-    pass
+
+
+
+    def remove(self, item, price, quantity=1):
+        for item_order in self.bill:
+            if item and price in item_order:
+                item_order["quantity"] -= quantity
+                if quantity == 0:
+                    del self.bill[item_order]
+                return True
+            else:
+                return False
